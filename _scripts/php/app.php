07e74e369c8fc2e5f4836ed8fc92cc3d3b99a9f7
Emilio Pagan-Yourno <Emilio.paganyourno1@gmail.com>
2013-05-06 00:08:20 -0400

    app.php update

--- _scripts/php/app.php
+++ _scripts/php/app.php
@@ -17,94 +17,27 @@ if (!$db_selected) {
 	die('Can\'t use ' . DB_NAME . ': ' . mysql_error()); 
 	} 
 	
-echo 'Connected succesfully';
+echo 'Connected succesfully'; 
 
-class app
-	{
-		private $dbconn;
-		private $tablename;
-		
-		function __construct( $tablename, $dbname, $dblogin, $dbpass,
-										$url = 'localhost' )
-		{
-         $this->dbconn = new PDO("mysql:host=$url;dbname=$dbname", "$dblogin", "$dbpass");
-         $this->tablename = $tablename;
-		}
+$value1 = $_POST['UserFirstName']; 
+$value2 = $_POST['UserMiddleName']; 
+$value3 = $_POST['UserLastName']; 
+$value4 = $_POST['UserAddress']; 
+$value5 = $_POST['UserCity']; 
+$value6 = $_POST['UserZip']; 
+$value7 = $_POST['UserDOB']; 
+$value8 = $_POST['UserSex']; 
+$value9 = $_POST['UserEyes']; 
+$value10 = $_POST['UserHeight']; 
 
-	   /**
-	    * Check whether desired name is available, return false if already taken.
-	    */
-	   function isUnameAvailable( $desired_uname )
-	   {
-	      // build query string
-	      $sql = 'SELECT Username FROM ' . $this->tablename . ' WHERE Username = "' . $desired_uname . '"';
+$sql = "INSERT INTO User (UserID, UserFirstName, UserMiddleName, 
+UserLastname, UserAddress, UserCity, UserZip, UserDOB, UserSex, UserEyes 
+UserHeight) VALUES ('', '$value1', '$value2', '$value3', '$value4', 
+'$value5', '$value6', '$value7', '$value8', '$value9', '$value10')"; 
 
-	      // submit database query
-	      $result = $this->dbconn->query( $sql );
+if (!mysql_query($sql)) { 
+	die('Error: ' . mysql_error()); 
+}
 
-	      return ( $result->fetch() == false );
-	   }
-
-	   /**
-	    * Insert new user info into the table.
-	    */
-	   function addAccount($uname, $passwd, $email)
-
-	   {
-	      /* simple MD5 encryption [for production, use something like crypt() instead]
-	       * NOTE: instead of PHP, we can encrypt via our SQL query directly
-	       *  [http://dev.mysql.com/doc/refman/5.5/en/encryption-functions.html]
-	       */ 
-	      $passwd = md5($passwd);
-
-	      // build INSERT query string
-	      $sql = 'INSERT INTO ' . $this->tablename . ' (Username, Password, Email) VALUES ("'
-	               . $uname . '", "' . $passwd . '", "' . $email . '")';
-
-	      // submit database query
-	      $result = $this->dbconn->exec( $sql );
-	   }
-
-	   /**
-	    * Lookup user name and check that entered password matches stored password.
-	    * Note: Our return value does not distinguish between the two possible
-	    * failures (uname not found or wrong password) because we want to give
-	    * as little info away as possible to a potential attacker.
-	    */
-	   function isLoginValid($uname, $passwd)
-	   {
-	      // get password value for row that matches the desired user name
-	      $sql = 'SELECT Username, Password FROM ' . $this->tablename . ' WHERE Username="' . $uname . '"';
-
-	      // submit database query
-	      $result = $this->dbconn->query( $sql );
-
-
-	      // get first result record (there should be only one)
-	      $entry = $result->fetch();
-
-	      // empty result set means login name not found
-	      if ( $entry == false )
-	         return false;
-
-	      /* simple MD5 encryption [for production, use something like crypt() instead]
-	       * NOTE: instead of PHP, we can encrypt via our SQL query directly
-	       *  [http://dev.mysql.com/doc/refman/5.5/en/encryption-functions.html]
-	       */ 
-	      $passwd = md5($passwd);
-
-	      // compare encrypted passwords
-
-	      return ( true );
-	   }
-	   /**
-   	 * Simple check whether both password form inputs match.
-   	 * A more complete implementation might also check for
-   	  other criteria, such as length, upper/lower case, etc.
-	    */
-   	function confirmPassword( $pass1, $pass2 )
-	   {
-   	   return $pass1 == $pass2;
-	   }
-	}
+mysql_close(); 
 ?>
